02a5e8f6550af6f413412da2012397ef8589fc9f
hartley9 <k.hartley1@lancaster.ac.uk>
2023-12-10 17:31:36 +0000

    update mumps shaders

--- mumpsShaders.js
+++ mumpsShaders.js
@@ -2,30 +2,29 @@ const mumpsShaders = [
 
     `uniform mat2 videoTransformMat2;\n\
     varying vec2 vUVvideo;\n\
-    // deformation 0 parameters:\n\
-    const vec2 TEARPOINT0 = vec2(-0.31,-0.62);\n\
-    const vec2 DISPLACEMENT0 = vec2(-0.22,-0.5);\n\
-    const float RADIUS0 = 0.3;\n\
 
-    // deformation 2 parameters:\n\
-    const vec2 TEARPOINT2 = vec2(-0.3, -0.62);\n\
-    const vec2 DISPLACEMENT2 = vec2(-0.23,-0.2);\n\
+    
+    // deformation 1 parameters:\n\
+    const vec2 TEARPOINT2 = vec2(-0.3, -0.72);\n\
+    const vec2 DISPLACEMENT2 = vec2(-0.2, -0.05);\n\
     const float RADIUS2 = 0.4;\n\
 
 
-    // deformation 3 parameters:\n\
-    const vec2 TEARPOINT3 = vec2(0.3, -0.62);\n\
-    const vec2 DISPLACEMENT3 = vec2(0.23,-0.2);\n\
+    // deformation 2 parameters:\n\
+    const vec2 TEARPOINT3 = vec2(0.3, -0.72);\n\
+    const vec2 DISPLACEMENT3 = vec2(0.2,-0.05);\n\
     const float RADIUS3 = 0.4;\n\
+
+    // main
     void main() {\n\
       vec3 positionDeformed=position;\n\
   
       
-      // apply deformation 2\n\
+      // apply deformation 1\n\
       float deformFactor2 = 1. - smoothstep(0.0, RADIUS2, distance(TEARPOINT2, position.xy));\n\
       positionDeformed.xy += deformFactor2 * DISPLACEMENT2;\n\
 
-      // apply deformation 3\n\
+      // apply deformation 2\n\
       float deformFactor3 = 1. - smoothstep(0.0, RADIUS3, distance(TEARPOINT3, position.xy));\n\
       positionDeformed.xy += deformFactor3 * DISPLACEMENT3;\n\
 
